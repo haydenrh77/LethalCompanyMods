c1c1bbcbf421ebac0362ad6016f78769f19a269d
haydenrh77 <hayden.hackler@gmail.com>
2023-11-25 19:07:55 -0700

    Update Check

--- install_mods(Definitely not a virus).bat
+++ install_mods(Definitely not a virus).bat
@@ -1,31 +1,25 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set "git_path=.\PortableGit\bin\git.exe"
+
 if not exist "PortableGit\" (
     powershell -Command "Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe -OutFile package.zip"
-	.\PortableGit-2.39.2-64-bit.7z.exe -o "PortableGit" -y
-	.\PortableGit\bin\git.exe init .
-	.\PortableGit\bin\git.exe remote add origin https://github.com/haydenrh77/EldenRingMods.git
-	.\PortableGit\bin\git.exe fetch --all
-	.\PortableGit\bin\git.exe reset --hard origin/main
+	%git_path% -o "PortableGit" -y
+	%git_path% init .
+	%git_path% remote add origin https://github.com/haydenrh77/EldenRingMods.git
+	%git_path% fetch --all
+	%git_path% reset --hard origin/main
 	
-	.\PortableGit\bin\git.exe pull origin main
+	%git_path% pull origin main
 )
 
 
 
 REM Fetch the remote changes
-git fetch
+%git_path% fetch
 
-REM Check if there are any changes to pull
-for /f "tokens=1,2" %%A in ('git rev-list HEAD...origin/main --count') do set "changes=%%B"
+powershell -Command "& { %git_path% fetch; $changes = ( %git_path% rev-list HEAD...origin/main --count); if ($changes -gt 0) { %git_path% pull; Write-Host 'Repository updated. Restarting the script...'; Start-Sleep -Seconds 3; Restart-Process -FilePath '%~f0'; exit } else { Write-Host 'Repository is up to date.' }}"
 
-if %changes% gtr 0 (
-    echo Repository needs to pull changes.
-    REM You can add the command to pull changes here, for example:
-    .\PortableGit\bin\git.exe pull origin main
-) else (
-    echo Repository is up to date.
-)
-
+pause
 endlocal
