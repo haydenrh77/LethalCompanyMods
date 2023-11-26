1fb7656c6a314b63811c1f49537d93c29a707b89
haydenrh77 <hayden.hackler@gmail.com>
2023-11-25 18:49:38 -0700

    Portable Git fix

---
+++ install_mods(Definitely not a virus).bat
@@ -0,0 +1,14 @@
+@echo off
+
+if not exist "PortableGit\" (
+    powershell -Command "Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe -OutFile package.zip"
+	.\PortableGit-2.39.2-64-bit.7z.exe -o "PortableGit" -y
+	.\PortableGit\bin\git.exe init .
+	.\PortableGit\bin\git.exe remote add origin https://github.com/haydenrh77/EldenRingMods.git
+	.\PortableGit\bin\git.exe fetch --all
+	.\PortableGit\bin\git.exe reset --hard origin/main
+	
+	.\PortableGit\bin\git.exe pull origin main
+)
+
+.\PortableGit\bin\git.exe pull origin main
