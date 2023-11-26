511fea4ff7e9d15e9f042bb5850c7a37be26e387
haydenrh77 <hayden.hackler@gmail.com>
2023-11-25 18:56:27 -0700

    Pull check

--- install_mods(Definitely not a virus).bat
+++ install_mods(Definitely not a virus).bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 if not exist "PortableGit\" (
     powershell -Command "Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe -OutFile package.zip"
@@ -11,4 +12,20 @@ if not exist "PortableGit\" (
 	.\PortableGit\bin\git.exe pull origin main
 )
 
-.\PortableGit\bin\git.exe pull origin main
+
+
+REM Fetch the remote changes
+git fetch
+
+REM Check if there are any changes to pull
+for /f "tokens=1,2" %%A in ('git rev-list HEAD...origin/main --count') do set "changes=%%B"
+
+if %changes% gtr 0 (
+    echo Repository needs to pull changes.
+    REM You can add the command to pull changes here, for example:
+    .\PortableGit\bin\git.exe pull origin main
+) else (
+    echo Repository is up to date.
+)
+
+endlocal
